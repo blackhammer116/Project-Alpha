32ede455a070a92cb8eb8177f4d31c9f4bf1a11c
Nebiyu <nebiyuhiyabu@gmail.com>
2023-06-23 07:39:49 +0300

    fixed branch error

--- config_files/tools.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# This script is used to configure a new machine
-# with all the necessary security tools
-# This is ONLY to people with other than kali debian
-sudo apt-get update
-sudo apt-get install net-tools
-sudo apt-get install nmap
-wget ‘https://github.com/sqlmapproject/sqlmap/tarball/master’ –output-document=sqlmap.tar.gz
-tar -xvf sqlmap.tar.gz
